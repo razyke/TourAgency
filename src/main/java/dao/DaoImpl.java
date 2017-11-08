acd287a1383dcd0c95f3dcdacc8a89771d044839
a-sakhnenko <aksakhnenko@gmail.com>
2017-11-08 11:19:24 +0300

    DAO for table "users", part 1

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,64 @@
+package dao;
+
+import model.Tour;
+import model.User;
+import model.UserMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+public class DaoImpl implements Dao {
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public User getUser(int id) {
+        String SQL = "SELECT * FROM users WHERE id = ?";
+        return jdbcTemplate.queryForObject(SQL, new Object[]{id}, new UserMapper());
+    }
+
+    public Collection<User> getAllUsers() {
+        String SQL = "SELECT * FROM users";
+        return jdbcTemplate.query(SQL, new UserMapper());
+    }
+
+    public void createUser(User user) {
+    }
+
+    public void deleteUser(int id) {
+        String SQL = "DELETE FROM users WHERE id = ?";
+        jdbcTemplate.update(SQL, new Object[]{id});
+    }
+
+    public void updateUser(User user) {
+    }
+
+    public Tour getTour(int id) {
+        return null;
+    }
+
+    public Collection<Tour> getAllTours() {
+        return null;
+    }
+
+    public void createTour(Tour tour) {
+
+    }
+
+    public void deleteTour(int id) {
+
+    }
+
+    public void updateTour(Tour tour) {
+
+    }
+}
