2bb8a1d616575ad5d02e1a18d1145846185a8931
a-sakhnenko <aksakhnenko@gmail.com>
2017-11-11 20:54:38 +0300

    Fix Beans.xml & StaticContextProvider

--- src/main/java/spring/StaticContextProvider.java
+++ src/main/java/spring/StaticContextProvider.java
@@ -1,10 +1,9 @@
 package spring;
 
-import dao.TourDao;
-import dao.UserDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import services.AuthService;
+import services.TourService;
 
 /**
  * It's required _only_ for servlets,
@@ -13,15 +12,11 @@ import services.AuthService;
 public class StaticContextProvider {
     private static final ApplicationContext CONTEXT = new ClassPathXmlApplicationContext("Beans.xml");
 
-    public static UserDao getUserDao() {
-        return (UserDao) CONTEXT.getBean("UserDao");
-    }
-
-    public static TourDao getTourDao() {
-        return (TourDao) CONTEXT.getBean("TourDao");
-    }
-
     public static AuthService getAuthService() {
         return (AuthService) CONTEXT.getBean("authService");
+    }
+
+    public static TourService getTourService() {
+        return (TourService) CONTEXT.getBean("tourService");
     }
 }
